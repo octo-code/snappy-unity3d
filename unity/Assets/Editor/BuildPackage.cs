7243445025ea253664135e20a2df8869a3255b50
TimL <tim@nerfgames.co.uk>
2015-01-14 15:28:05 +0000

    BuildPackage now pulls snappy code into editor (doing it this way there isn't duplication of snappy src in repo)

--- unity/Assets/Editor/BuildPackage.cs
+++ unity/Assets/Editor/BuildPackage.cs
@@ -16,8 +16,27 @@ public class BuildPackage
 	[MenuItem("Build/Snappy/Build Package")]
 	public static void BuildUnityPackage()
 	{
+		string lIOSPluginsPath = "Assets/Editor/Snappy/iOS/src";
+
+		Directory.CreateDirectory(lIOSPluginsPath);
+
+		File.Copy("../snappy/snappy.cc",                Path.Combine(lIOSPluginsPath, "snappy.cc"), true);
+		File.Copy("../snappy/snappy.h",                 Path.Combine(lIOSPluginsPath, "snappy.h"), true);
+		File.Copy("../snappy/snappy-c.cc",              Path.Combine(lIOSPluginsPath, "snappy-c.cc"), true);
+		File.Copy("../snappy/snappy-c.h",               Path.Combine(lIOSPluginsPath, "snappy-c.h"), true);
+		File.Copy("../snappy/snappy-internal.h",        Path.Combine(lIOSPluginsPath, "snappy-internal.h"), true);
+		File.Copy("../snappy/snappy-sinksource.cc",     Path.Combine(lIOSPluginsPath, "snappy-sinksource.cc"), true);
+		File.Copy("../snappy/snappy-sinksource.h",      Path.Combine(lIOSPluginsPath, "snappy-sinksource.h"), true);
+		File.Copy("../snappy/snappy-stubs-internal.cc", Path.Combine(lIOSPluginsPath, "snappy-stubs-internal.cc"), true);
+		File.Copy("../snappy/snappy-stubs-internal.h",  Path.Combine(lIOSPluginsPath, "snappy-stubs-internal.h"), true);
+		File.Copy("../snappy/snappy-stubs-public.h",    Path.Combine(lIOSPluginsPath, "snappy-stubs-public.h"), true);
+
 		AssetDatabase.Refresh();
 
 		AssetDatabase.ExportPackage(m_PackageInputPaths, m_PackageOutputPath, ExportPackageOptions.Recurse);
+
+		Directory.Delete(lIOSPluginsPath, true);
+
+		AssetDatabase.Refresh();
 	}
 }
